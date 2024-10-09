9b1b6fe7e7de70b18efba0ca5162a27763bf46e2
TJNyland2 <AQ242209@aquinas.ac.uk>
2024-10-09 22:53:21 +0100

    Add project files.

---
+++ TimerTablesTester/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace TimerTablesTester
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool playing = true;
+            while (playing == true)
+            {
+                Console.WriteLine("If you want to exit Time Table Tester - Enter `X`, If not press any other key."); //if you want to exit before a new game
+                string exit = Console.ReadLine(); //stores
+                if (exit == "X") //if you want to exit
+                { 
+                    Console.WriteLine("Goodbye!");
+                    playing = false; //stops loop
+                    break; //breaks out of loop
+                }
+                Console.WriteLine("Welcome!");
+                Console.WriteLine("What is your name? "); //input name
+                string name = Console.ReadLine(); //stores name
+                Console.WriteLine(name + " what times table would you like to learn?"); //asks what time tables
+                int timetable = int.Parse(Console.ReadLine()); //stores answer
+                if (timetable >= 1 && timetable <= 12) //checks if answer is 1-12
+                {
+                int score = TimeTables(timetable); //runs the int method 
+                    if (score <= 5)
+                    {
+                        Console.WriteLine("You need more practice"); 
+                        Console.WriteLine("You scored: " + score + "/10"); //score is returned to here
+                    }
+                    else if (score <= 8)
+                    {
+                        Console.WriteLine("Thats pretty good!");
+                        Console.WriteLine("You scored: " + score + "/10"); //score is returned to here
+                    }
+                    else if (score >= 9)
+                    {
+                        Console.WriteLine("Excellent");
+                        Console.WriteLine("You scored: " + score + "/10"); //score is returned to here
+                    }
+                 
+          
+                     }
+                else //if not 1-12
+                {
+                  Console.WriteLine("An error has occured :( (invaid input)"); //error
+                   return;//returns to the loop
+                 }
+               Console.ReadLine(); //reads all the lines!
+            }
+           
+        }
+        static int TimeTables(int timetable) //Method TimeTables
+        {
+            int score = 0; //value of score
+            for (int i = 0; i < 10; i++)//For loop for 10 questions
+            {
+               
+                Random r = new Random(); //imports random
+                int RandNumber = r.Next(1, 13); // Generates random number
+                Console.WriteLine("What is " + RandNumber + "x" + timetable); //The question
+                int answer = int.Parse(Console.ReadLine()); //stores answer
+                int realAnswer = (timetable * RandNumber); //gets the answer
+                if (answer == realAnswer) //checks the answer is correct
+                {
+                   Console.WriteLine("Well done you got it correct!");
+                    score++; //adds to score as it is correct
+                    //Console.WriteLine(score);
+                }
+                else
+                {
+                    Console.WriteLine("Sorry you got it incorrect!"); //says it is incorrect
+                }
+            }
+            return score;
+
+
+
+        }
+        
+    }
+}
---
+++ TimerTablesTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TimerTablesTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TimerTablesTester")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a34abd72-9d78-4f32-b4c4-acb28f476b7f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
